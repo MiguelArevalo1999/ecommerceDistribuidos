3903a9ef1887b58cfe7214a094e79070853952b0
Miguel <marevaloa1400@alumno.ipn.mx>
2022-06-07 01:08:10 -0500

    Agregar archivos de proyecto.

---
+++ ecommerceDistribuidos/Models/Articulos.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ecommerceDistribuidos.Models
+{
+    public class Articulos
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public string Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public string Fotografia { get; set; }
+
+
+    }
+}
